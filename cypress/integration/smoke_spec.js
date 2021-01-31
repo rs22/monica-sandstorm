9a1793d5d67839f73e18294cd9f5849267364f99
Robert Schmid <r.schmid@outlook.com>
2021-01-31 22:40:42 +0100

    Smoke test: dismiss tooltip

--- cypress/integration/smoke_spec.js
+++ cypress/integration/smoke_spec.js
@@ -1,25 +1,3 @@
-const getGrainFrameDocument = () => {
-    return cy
-    .get('iframe.grain-frame')
-    // Cypress yields jQuery element, which has the real
-    // DOM element under property "0".
-    // From the real DOM iframe element we can get
-    // the "document" element, it is stored in "contentDocument" property
-    // Cypress "its" command can access deep properties using dot notation
-    // https://on.cypress.io/its
-    .its('0.contentDocument').should('exist')
-  }
-
-  const getGrainFrameBody = () => {
-    // get the document
-    return getGrainFrameDocument()
-    // automatically retries until body is loaded
-    .its('body').should('not.be.undefined')
-    // wraps "body" DOM element to allow
-    // chaining more Cypress commands, like ".find(...)"
-    .then(cy.wrap)
-  }
-
 describe('Smoke Test', () => {
     it('creates a grain, performs setup and creates a contact', () => {
         cy.visit('http://local.sandstorm.io:6080')
@@ -28,6 +6,7 @@ describe('Smoke Test', () => {
         cy.contains('with a Dev account').click()
         cy.contains('Alice (admin)').click()
         cy.contains('Continue').click()
+        cy.contains('Got It').click()
 
         // Create grain
         cy.contains('Monica').click()
